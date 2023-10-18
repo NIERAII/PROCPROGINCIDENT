b93088e19847b7f72f6c248ea698c9dc2a3a02d7
Fenire Dein <Pavlkost@mail.ru>
2023-10-19 00:21:25 +0300

    added hw4 tasks 1-6

--- HW C++/ifelseincident.cpp
+++ HW C++/ifelseincident.cpp
@@ -5,6 +5,9 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <Windows.h>
+#include <map>
+
 using namespace std;
 float ln(float x) {
     //https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BB%D0%BE%D0%B3%D0%B0%D1%80%D0%B8%D1%84%D0%BC
@@ -14,6 +17,84 @@ float ln(float x) {
 float calculateY(float x) {
     return (x * x - 2 * x + 2) / (x - 1);
 } // https://discord.com/channels/@me/1075531659451056259/1150796298585382942
+
+int sign(int x) {
+    if (x > 0) {
+        return 1;
+    }
+    else if (x == 0) {
+        return 0;
+    }
+    else if (x < 0) {
+        return -1;
+    }
+}
+
+#define WIDTHs 80
+#define HEIGHTs 24
+
+
+
+void setCursorPosition(int x, int y) {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(hOut, coord);
+}
+
+void drawGraph() {
+    double y;
+    int x;
+
+    for (x = 0; x < WIDTHs; x++) {
+        y = sin(2 * 3.14159 * x / WIDTHs);
+
+        int plotY = (int)((y + 1) * 0.5 * HEIGHTs);
+
+        setCursorPosition(x, plotY);
+        cout << '*';
+    }
+}
+
+
+double calculateRectangleArea(double length, double width) {
+    return length * width;
+}
+
+double calculateTriangleArea(double base, double height) {
+    return 0.5 * base * height;
+}
+
+double calculateCircleArea(double radius) {
+    return 3.14 * pow(radius, 2);
+}
+
+
+int romanToInt(string s) {
+    map<char, int> romanValues = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int result = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        int current = romanValues[s[i]];
+
+        if (i < s.length() - 1 && current < romanValues[s[i + 1]]) {
+            result -= current;
+        }
+        else {
+            result += current;
+        }
+    }
+
+    return result;
+}
 
 int main()
 {
@@ -253,9 +334,128 @@ int main()
                 alphabet.push_back(ch);
         }
         sort(alphabet.begin(), alphabet.end());
-        cout << alphabet << "\n";
+        cout << alphabet << "\n\n";
 
 
+
+    }
+    else if (hw == 4) {
+        cout << "HW #4\n\n";
+        cout << "Task1\n\n";
+        ofstream file("numbers.txt"); // создаем файл для записи
+
+        if (file.is_open()) {
+            cout << "Input 10 numbers: " << endl;
+
+            int number;
+            for (int i = 0; i < 10; i++) {
+               cin >> number;
+                file << number <<endl; // записываем число в файл
+            }
+
+            file.close(); // закрываем файл
+
+           ifstream inputFile("numbers.txt"); // открываем файл для чтения
+
+            if (inputFile.is_open()) {
+                int sum = 0;
+                int value;
+
+                while (inputFile >> value) {
+                    sum += value; // суммируем числа из файла
+                }
+
+                inputFile.close(); // закрываем файл
+
+               cout << "Addition result: " << sum <<endl << endl;
+            }
+            else {
+               cout << "Err." <<endl << endl;
+            }
+        }
+        else {
+           cout << "Err." <<endl << endl;
+        }
+
+        cout << "Task2\n\n";
+        int input;
+        cout << "Int1\n";
+        cin >> input;
+        cout << sign(input) << endl << endl;
+        
+        cout << "Task3\n\n";
+
+        int choice;
+        double result;
+
+        cout << "Choose shape:\n";
+        cout << "1. Rectangle\n";
+        cout << "2. Triangle\n";
+        cout << "3. Circle\n";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            double length, width;
+            cout << "Length n Width: ";
+            cin >> length >> width;
+            result = calculateRectangleArea(length, width);
+            break;
+        case 2:
+            double base, height;
+            cout << "Base n Height: ";
+            cin >> base >> height;
+            result = calculateTriangleArea(base, height);
+            break;
+        case 3:
+            double radius;
+            cout << "Radius: ";
+            cin >> radius;
+            result = calculateCircleArea(radius);
+            break;
+        default:
+            cout << "Invalid.\n\n";
+            return 0;
+        }
+
+        cout << "S: " << result << endl << endl;
+        
+        cout << "Task4\n\n";
+        int numStars = 36;
+        int numStripes = 8;
+
+        for (int i = 1; i <= numStripes / 2; i++) {
+            for (int j = 1; j <= numStars; j++) {
+                if (j <= numStars / 3) {
+                    cout << "*";
+                }
+                else {
+                    cout << "_";
+                }
+            }
+            cout << endl;
+        }
+        for (int i = 1; i <= numStripes / 2; i++) {
+            for (int j = 1; j <= numStars; j++) {
+                if (j <= numStars) {
+                    cout << "_";
+                }
+            }
+            cout << endl;
+        }
+
+        cout << "\n\nTask5\n\n";
+        drawGraph();
+
+        cout << "Task6\n\n";
+
+        string romanNumeral;
+        cout << "Roman Number: ";
+        cin >> romanNumeral;
+
+        int arabicNumeral = romanToInt(romanNumeral);
+
+        cout << "Arabic Number: " << arabicNumeral << endl;
 
     }
     else { cout << "There is no such HW"; }
